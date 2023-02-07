03503c95277670cac4f1f2d361e906a18db0cf27
Aurelie Fischer <aurelie.fischer@chu-lyon.fr>
2023-02-07 16:34:42 +0100

    Added circlator to Illumina-only pipeline

--- bin/spades.sh
+++ bin/spades.sh
@@ -6,14 +6,18 @@ SAMPLE=$1
 SAMPLE_R1="$SAMPLE""_R1.fastq.gz"
 SAMPLE_R2="$SAMPLE""_R2.fastq.gz"
 SAMPLE_POLISHED="$SAMPLE""_polished"
+SAMPLE_POLISHED_REALIGNED="$SAMPLE""_polished_realigned"
 
 cd $SAMPLE
 mkdir spades
 mkdir quast
+mkdir circlator
 
-spades.py -1 $SAMPLE_R1 -2 $SAMPLE_R2 --isolate -o spades \
+spades.py -1 $SAMPLE_R1 -2 $SAMPLE_R2 --isolate -o spades -t 48 \
     1> spades/spades_1.log 2> spades/spades.err && \
-cp spades/contigs.fasta $SAMPLE_POLISHED && \
-quast.py -o quast $SAMPLE_POLISHED \
+cp spades/contigs.fasta ./"$SAMPLE_POLISHED".fasta && \
+quast.py -o quast ./"$SAMPLE_POLISHED".fasta \
     1> quast/quast.log \
-    2> quast/quast.err 
+    2> quast/quast.err && \
+circlator fixstart "$SAMPLE_POLISHED".fasta circlator/"$SAMPLE_POLISHED_REALIGNED" && \
+cp circlator/$SAMPLE_POLISHED_REALIGNED.fasta ./"$SAMPLE_POLISHED_REALIGNED".fasta
