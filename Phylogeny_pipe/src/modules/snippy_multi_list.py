0f342e704465d5605a7d757926cd3394e7fd1d16
Aurelie Fischer <aurelie.fischer@chu-lyon.fr>
2025-08-11 15:16:15 +0200

    Directed to R1 and R2 files that are correctly resynced

--- Phylogeny_pipe/src/modules/snippy_multi_list.py
+++ Phylogeny_pipe/src/modules/snippy_multi_list.py
@@ -13,9 +13,9 @@ def main(work_dir, samples_list, out_dir):
     out_file = open(os.path.join(out_dir, "input.tab"), "w")
     for sample in samples_list:
         sample_dir = os.path.join(out_dir, sample)
-        R1 = os.path.join(genomes_dir, sample, "trimmomatic", sample+"_R1_paired.trimmed.fastq.gz")
-        R2 = os.path.join(genomes_dir, sample, "trimmomatic", sample+"_R2_paired.trimmed.fastq.gz")
-        out_file.write(sample_dir+"\t"+R1+"\t"+R2+"\n")    
+        R1 = os.path.join(genomes_dir, sample, "trimmomatic", sample+"_R1_paired.trimmed.resync.fastq.gz")
+        R2 = os.path.join(genomes_dir, sample, "trimmomatic", sample+"_R2_paired.trimmed.resync.fastq.gz")
+        out_file.write(sample_dir+"\t"+R1+"\t"+R2+"\n")
     out_file.close()
 
 if __name__ == '__main__':
