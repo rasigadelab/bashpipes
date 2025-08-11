e84f72324df02a2e065aee6e6ad6cfb91649853a
Aurelie Fischer <aurelie.fischer@chu-lyon.fr>
2025-08-11 11:54:24 +0200

    Disabled nextflow search for updates when initializing runs

--- Illumina_only_pipe/launch_pipeline.sh
+++ Illumina_only_pipe/launch_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nextflow -C src/nextflow.config run src/main.nf -params-file src/params_illumina_only.json -with-trace -with-report --prefix run_illumina_only -profile standard
+NXF_OFFLINE=true NXF_DISABLE_AUTO_UPDATE=true nextflow -C src/nextflow.config run src/main.nf -params-file src/params_illumina_only.json -with-trace -with-report --prefix run_illumina_only -profile standard
 
