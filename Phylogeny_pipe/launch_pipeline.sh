f8db3eafb0aac3fe07a87bb7c4dfc3bc1ad6468c
Aurelie Fischer <aurelie.fischer@chu-lyon.fr>
2025-08-11 15:12:34 +0200

    Disabled Nextflow auto updates

--- Phylogeny_pipe/launch_pipeline.sh
+++ Phylogeny_pipe/launch_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-nextflow -C src/nextflow.config run src/main.nf -params-file src/params_mash.json -with-trace -with-report --prefix run_1 -profile standard
+NXF_OFFLINE=true NXF_DISABLE_AUTO_UPDATE=true nextflow -C src/nextflow.config run src/main.nf -params-file src/params_mash.json -with-trace -with-report --prefix run_1 -profile standard
 
-# nextflow -C src/nextflow.config run src/main.nf -params-file src/params_variant_calling.json -with-trace -with-report --prefix run_1 -profile standard
+NXF_OFFLINE=true NXF_DISABLE_AUTO_UPDATE=true nextflow -C src/nextflow.config run src/main.nf -params-file src/params_variant_calling.json -with-trace -with-report --prefix run_1 -profile standard
 
-# nextflow -C src/nextflow.config run src/main.nf -params-file src/params_phylogeny.json -with-trace -with-report --prefix run_1 -profile standard
+NXF_OFFLINE=true NXF_DISABLE_AUTO_UPDATE=true nextflow -C src/nextflow.config run src/main.nf -params-file src/params_phylogeny.json -with-trace -with-report --prefix run_1 -profile standard
