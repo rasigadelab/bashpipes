85f9796b7d27da56f83450a232dfe0fb4057be13
Aurelie Fischer <aurelie.fischer@chu-lyon.fr>
2025-08-11 15:03:46 +0200

    Disabled automatic Nextflow updates

--- Nano_Illumina_pipe/launch_pipeline.sh
+++ Nano_Illumina_pipe/launch_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nextflow -C src/nextflow.config run src/main.nf -params-file src/params_nano_illumina.json -with-trace -with-report --prefix run_nano_illumina -profile standard
+NXF_OFFLINE=true NXF_DISABLE_AUTO_UPDATE=true nextflow -C src/nextflow.config run src/main.nf -params-file src/params_nano_illumina.json -with-trace -with-report --prefix run_nano_illumina -profile standard
 
